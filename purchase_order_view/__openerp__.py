206ec04d73bea34e86885668eb4611ab904e3fc9
janicengyy <janice@roomsfor.hk>
2014-12-03 10:38:38 +0800

    Change module name
    Rename module to Purchase Order View

--- purchase_order_view/__openerp__.py
+++ purchase_order_view/__openerp__.py
@@ -1,12 +1,12 @@
 {
-    "name" : "New tree views",
+    "name" : "Purchase Order View",
     "version" : "1.0",
     "category" : "Generic Modules Control",
     'depends' : ['account','base','base_setup','product','sale','sale_order_dates'],
     'description': """
 Functions:
-add menu items:
-
+==================================================
+* Add menu items under Purchases to show more details of Purchase Orders in list view.
     """,
     
     "data" : ["purchase_order_tree.xml"],
